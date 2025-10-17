6257a3981388b4744eceb74b67c4e7a1ce55e7fd
Azhaan Ali Siddiqui <azhaanalisiddiqui15@gmail.com>
2025-10-17 22:53:12 +0530

    auth and user backend js modified

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const User = require('./models/User');
-const Course = require('./models/Course');
+const User = require('../models/User');
+const Course = require('../models/Course');
 const { protect } = require('../middleware/auth');
 
 // @route   PUT /api/users/points
